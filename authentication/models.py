7138c156929386c3c1ef02eb242c68ebb1b486e3
mdfaiz903 <mdfaiz.nub@gmail.com>
2024-03-23 19:34:17 +0600

    project setup & authentication app creation done and all functionality added

---
+++ authentication/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from rest_framework.authtoken.models import Token
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('administrator', 'Administrator'),
+        ('teacher', 'Teacher'),
+        ('student', 'Student'),
+        ('staff', 'Staff'),
+    )
+
+    role = models.CharField(max_length=15, choices=ROLE_CHOICES)
--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from users.views import UserRegistrationView, UserLoginView, UserLogoutView
-
-urlpatterns = [
-    path('api/auth/register/', UserRegistrationView.as_view(), name='user-registration'),
-    path('api/auth/login/', UserLoginView.as_view(), name='user-login'),
-    path('api/auth/logout/', UserLogoutView.as_view(), name='user-logout'),
-    # Add other URLs here
-]
